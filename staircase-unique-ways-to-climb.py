e40c2d3a9d430baf8f98b71bf48b3061652e0373
sujanshrestha <shrestha94.sujan@gmail.com>
2020-08-24 07:17:12 -0500

    You are given a positive integer N which represents the number of steps in a staircase. You can either climb 1 or 2 steps at a time. Write a function that returns the number of unique ways to climb the stairs.

---
+++ staircase-unique-ways-to-climb.py
@@ -0,0 +1,15 @@
+class Solution:
+    """You are given a positive integer N which represents the number of steps in a staircase.
+      You can either climb 1 or 2 steps at a time.
+      Write a function that returns the number of unique ways to climb the stairs."""
+
+    @staticmethod
+    def staircase(n):
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().staircase(4))
+    # 5
+    print(Solution().staircase(5))
+    # 8
