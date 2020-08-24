bbd27f22c9c92c689dc31a8429e0d2211b7ae048
sujanshrestha <shrestha94.sujan@gmail.com>
2020-08-24 07:16:25 -0500

    Given a list of numbers, find if there exists a pythagorean triplet in that list. A pythagorean triplet is 3 variables a, b, c where a2 + b2 = c2

---
+++ find-if-list-has-pythagorean-triplets.py
@@ -0,0 +1,11 @@
+class Solution:
+    """Given a list of numbers, find if there exists a pythagorean triplet in that list.
+     A pythagorean triplet is 3 variables a, b, c where a2 + b2 = c2"""
+
+    @staticmethod
+    def findPythagoreanTriplets(nums):
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().findPythagoreanTriplets([3, 12, 5, 13]))
